ab5906773097140fd711c294bffb273d3c0d89c2
jetro <semak-v@ukr.net>
2020-04-18 19:31:15 +0300

    added upload page

--- app/js/main.js
+++ app/js/main.js
@@ -43,6 +43,8 @@ $(function () {
             return false;
         });
 
+        $('input[type="file"], select').styler(); 
+
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
