10ec20c3ac9a9f2f4c3ee2ea8309174df6175374
vovkasem <semak-v@ukr.net>
2022-06-29 10:31:52 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
-const browserSync  = require('browser-sync').create();
-const uglify       = require('gulp-uglify-es').default;
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 
 function browsersync() {
     browserSync.init({
-        server : {
+        server: {
             baseDir: 'app/'
         }
     });
@@ -22,20 +22,20 @@ function cleanDist() {
 
 function images() {
     return src('app/images/**/*')
-    .pipe(imagemin(
-        [
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]
-    ))
-    .pipe(dest('dist/images'))
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest('dist/images'))
 }
 
 function scripts() {
@@ -43,16 +43,18 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'app/js/main.js'
     ])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
 }
 
-
 function styles() {
-    return src('app/scss/style.scss')
-        .pipe(scss({outputStyle: 'compressed'}))
+    return src([
+        'node_modules/normalize.css/normalize.css',
+        'app/scss/**/*.scss',
+    ])
+        .pipe(scss({ outputStyle: 'compressed' }))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 10 version'],
@@ -68,8 +70,8 @@ function build() {
         'app/fonts/**/*',
         'app/js/main.min.js',
         'app/*.html'
-    ], {base: 'app'})
-    .pipe(dest('dist'))
+    ], { base: 'app' })
+        .pipe(dest('dist'))
 }
 
 function watching() {
